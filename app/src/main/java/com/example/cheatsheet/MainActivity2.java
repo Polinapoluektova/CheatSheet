d5bcfcd565d86c478a33211d23277874ac89b6a6
poluektovapolina12@gmail.com <poluektovapolina12@gmail.com>
2021-10-15 23:21:59 +0300

    Task

---
+++ app/src/main/java/com/example/cheatsheet/MainActivity2.java
@@ -0,0 +1,34 @@
+package com.example.cheatsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_second)
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(26);
+        textView.setPadding(16, 16, 16, 16);
+
+        Bundle arguments = getIntent().getExtras();
+
+        User user;
+        if(arguments!=null){
+            user = (User) arguments.getSerializable(User.class.getSimpleName());
+
+            textView.setText("Name: " + user.getText());
+        }
+        setContentView(textView);
+    }
+}
